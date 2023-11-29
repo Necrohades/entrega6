e946b0f57ae3633f246498b171c113d46d372761
Frankwii <frank.william.hammond@gmail.com>
2023-11-29 19:27:59 +0100

    Inicio

--- main.R
+++ main.R
@@ -1,3 +1,11 @@
-df_pm=read.table("ANIMALS-matrixPM.txt")
+library(MVA)
+library(tidyverse)
+read.table("ANIMALS-matrixPM.txt")-> PM
+read.table("ANIMALS-matrixSP.txt")-> SP
+read.table("ANIMALS-matrixVH.txt")-> VH
 
+
+PM_cmd<-cmdscale(PM,k=2)
+plot(PM_cmd, type="n")
+text(PM_cmd, labels="")
 
