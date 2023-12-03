874f52ef67247e5ae1c552d822066310359edbf3
Maria del Mar Deyà <mar2002deya@gmail.com>
2023-12-03 16:53:42 +0100

    agrupar phylum

--- main.R
+++ main.R
@@ -1,28 +1,32 @@
 library(MVA)
 library(tidyverse)
 library(ggplot2)
-
+read.table("fileListANIMAL_phylum.txt") ->phylum
 read.table("ANIMALS-matrixPM.txt")-> PM
+sqrt(2* (1-PM)) -> PM
 read.table("ANIMALS-matrixSP.txt")-> SP
+sqrt(2* (1-SP)) -> SP
 read.table("ANIMALS-matrixVH.txt")-> VH
+sqrt(2* (1-VH)) -> VH
 
+Phylum = phylum[,2] %>%  as.factor
 
 PM_cmd <- cmdscale(PM,k=2) %>% as.data.frame
 
-PM_cmd %>% ggplot(aes(x = V1, y = V2)) +
+PM_cmd %>% ggplot(aes(x = V1, y = V2, color = Phylum)) +
   geom_point() +
   ggtitle("PM kernel")
 
 
 SP_cmd <- cmdscale(SP,k=2) %>% as.data.frame
 
-SP_cmd %>% ggplot(aes(x = V1, y = V2)) +
+SP_cmd %>% ggplot(aes(x = V1, y = V2, color = Phylum)) +
   geom_point() +
   ggtitle("SP kernel")
 
 
 VH_cmd <- cmdscale(VH,k=2) %>% as.data.frame
 
-VH_cmd %>% ggplot(aes(x = V1, y = V2)) +
-  geom_point() +
+VH_cmd %>% ggplot(aes(x = V1, y = V2, color = Phylum)) +
+  geom_point(color = grupos_phylum) +
   ggtitle("VH kernel")
