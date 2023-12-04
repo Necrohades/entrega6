a72ab863b5174e8c4d6e2f7af8d52aca4eff4398
Necrohades <tfuentes2004@gmail.com>
2023-12-04 10:01:33 +0100

    Vull fer pull

--- main.R
+++ main.R
@@ -6,23 +6,30 @@ read.table("ANIMALS-matrixPM.txt")-> PM
 read.table("ANIMALS-matrixSP.txt")-> SP
 read.table("ANIMALS-matrixVH.txt")-> VH
 
+phylum <- read.table("fileListANIMAL_phylum.txt", header = FALSE)
+colnames(phylum) <- c("Animal", "Phylum")
+phylum <- phylum %>% mutate(Phylum = as.factor(Phylum))
+
 
 PM_cmd <- cmdscale(PM,k=2) %>% as.data.frame
+PM_cmd <- merge(PM_cmd, phylum)
 
-PM_cmd %>% ggplot(aes(x = V1, y = V2)) +
+PM_cmd %>% ggplot(aes(x = V1, y = V2, color = Phylum)) +
   geom_point() +
   ggtitle("PM kernel")
 
 
 SP_cmd <- cmdscale(SP,k=2) %>% as.data.frame
+SP_cmd <- merge(SP_cmd, phylum)
 
-SP_cmd %>% ggplot(aes(x = V1, y = V2)) +
+SP_cmd %>% ggplot(aes(x = V1, y = V2, color = Phylum)) +
   geom_point() +
   ggtitle("SP kernel")
 
 
 VH_cmd <- cmdscale(VH,k=2) %>% as.data.frame
+VH_cmd <- merge(VH_cmd, phylum)
 
-VH_cmd %>% ggplot(aes(x = V1, y = V2)) +
+VH_cmd %>% ggplot(aes(x = V1, y = V2, color = Phylum)) +
   geom_point() +
   ggtitle("VH kernel")
